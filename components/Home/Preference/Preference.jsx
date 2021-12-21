4af41d216fbf4b28c8f4fc41fba3c0b8ef7c92b5
iazadur <iamazadur@gmail.com>
2021-12-21 19:26:04 +0600

    preference

--- components/Home/Preference/Preference.jsx
+++ components/Home/Preference/Preference.jsx
@@ -10,8 +10,10 @@ const Preference = () => {
                     <div className=""><span className='rounded-2xl border border-gray-600 text-pink-600 px-2 '>Add Your Post</span></div>
                 </div>
 
+                <div className="px-3">
 
-                {Array.from({ length: 4 }).map((_, idx) => (<SinglePreference key={idx} />))}
+                    {Array.from({ length: 4 }).map((_, idx) => (<SinglePreference key={idx} />))}
+                </div>
             </div>
 
         </>
--- components/Home/Preference/SinglePreference.jsx
+++ components/Home/Preference/SinglePreference.jsx
@@ -5,13 +5,14 @@ const SinglePreference = () => {
     return (
         <div className='flex items-center justify-start gap-4 my-2'>
             <div className="">
-            <Image src="/img/azad.jpg" className='rounded-full' width="80" height="80" alt='' />
+            <Image src="/img/azad.jpg" className='rounded-full' width="100" height="100" alt='' />
             </div>
             <div className="flex flex-col">
-                <p>hello</p>
-                <p>hello</p>
-                <p>hello</p>
-                <p>hello</p>
+                <p>Md Azadur Rahman</p>
+                <p>{"22 yer's,5`8``, Private Job, Unmarried, Dhaka"} </p>
+                <p>Seeking educated, serious</p>
+                <p>tuitable lifePartner</p>
+               <div className=""><span className="rounded-2xl border border-gray-500 p-0 m-0 px-2">See More</span></div>
                 
             </div>
         </div>
