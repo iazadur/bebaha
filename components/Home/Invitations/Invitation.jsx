24261d5afd4fe96a52de6031d8cf41a4f43f54e3
iazadur <iamazadur@gmail.com>
2021-12-22 14:39:16 +0600

    invitations icons design problem

--- components/Home/Invitations/Invitation.jsx
+++ components/Home/Invitations/Invitation.jsx
@@ -16,8 +16,11 @@ const Invitation = () => {
                 style={{ width: 250 }}
                 cover={<Image src="/img/azad.jpg" width="250" height="300" alt='' />}
                 actions={[
-                    <CloseOutlined key="reject" />,
-                    <CheckOutlined key="done" />,
+                    <div className="flex flex-col gap-x-2 justify-center items-center text-gray-400 hover:text-green-400" key="reject">
+                        <CloseOutlined className="rounded-full border hover:border-green-500 border-gray-500 p-2"  />
+                        <p>Decline</p>
+                        </div>,
+                    <div className="flex flex-col gap-x-2 justify-center items-center text-gray-400 hover:text-green-400" key="accept"><CheckOutlined className="rounded-full border hover:border-green-500 border-gray-500 p-2"  /><p>Accept</p></div>,
                 ]}
             >
                 <Meta className='text-center' title="Md Azadur" description="22 yr's, 5`7``, Dhaka, Unmmared" />
