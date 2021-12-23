d3d49a4c657b9242775211eb4cf77060f2f3e2ef
iazadur <iamazadur@gmail.com>
2021-12-23 15:13:17 +0600

    update invitations

--- components/Home/Users/Users.jsx
+++ components/Home/Users/Users.jsx
@@ -5,12 +5,12 @@ const Users = () => {
     return (
         <>
             <div className="my-3">
-                <div className="flex justify-between items-center px-3">
+                <div className="flex justify-between items-center px-4">
                     <div className=""><span>Top 200 profiles</span></div>
                     
                 </div>
 
-                <div className="md:px-0 mx-2 grid grid-cols-1 my-2 gap-x-2 gap-y-2 ">
+                <div className="md:px-4 mx-auto">
 
                     {Array.from({ length: 1 }).map((_, idx) => (<User key={idx} />))}
                 </div>
